bda73a1bfcafa2e4a3236979be8ce2069d3d97c2
SycaJay <jessica.yumu@ashesi.edu.gh>
2026-05-25 22:50:37 +0000

    Update verify_payment.php

--- actions/verify_payment.php
+++ actions/verify_payment.php
@@ -5,7 +5,7 @@ header('Pragma: no-cache');
 header('Expires: 0');
 
 // Replace with your actual Paystack secret key
-$secret_key = 'sk_live_d35382a21a03bd28a6ed4bc0417152c06289a135'; // TODO: Update with your actual LIVE Paystack secret key
+$secret_key = '..'; // TODO: Update with your actual LIVE Paystack secret key
 
 // Get the raw POST data
 $input = file_get_contents('php://input');
